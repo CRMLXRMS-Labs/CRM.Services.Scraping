f4488395bc5c3cfe02808a6d2bcec4d9e8daaa1d
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-16 01:47:07 +0200

    (#1) ci: udpate ci

--- src/CRMScraper.Tests/obj/Debug/net8.0/CRMScraper.Tests.AssemblyInfo.cs
+++ src/CRMScraper.Tests/obj/Debug/net8.0/CRMScraper.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRMScraper.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d172efcf815f5a73c7f4508736a30a92a6e8c11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d590aea27f42d25820ac2a460d9d64986089c270")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRMScraper.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRMScraper.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
