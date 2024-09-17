a43dfb3ccc9f2c2f078f0d6e0e07aaae85cbc321
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-18 00:51:50 +0200

    (#10) repo: issue responder

--- scripts/publish_nuget.sh
+++ scripts/publish_nuget.sh
@@ -9,14 +9,25 @@ echo Restoring NuGet packages...
 dotnet restore
 
 echo Packing the library...
-dotnet pack -c release /p:PackageVersion=1.1.$GITHUB_RUN_NUMBER --no-restore -o .
+dotnet pack -c release /p:PackageVersion=1.1.$GITHUB_RUN_NUMBER --no-restore -o ./nupkg
 
 echo Library package created for branch $GITHUB_REF_NAME
 
 case "$GITHUB_REF_NAME" in
   "main")
     echo Uploading CRMScraper.Library package to NuGet
-    dotnet nuget push *.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
+
+    # Define the path to the package
+    PACKAGE_PATH="./nupkg/CRMScraper.Library.1.1.$GITHUB_RUN_NUMBER.nupkg"
+
+    # Check if the package exists before attempting to push
+    if [ -f "$PACKAGE_PATH" ]; then
+      echo "Package found: $PACKAGE_PATH"
+      dotnet nuget push "$PACKAGE_PATH" -k "$NUGET_API_KEY" -s https://api.nuget.org/v3/index.json
+    else
+      echo "Error: Package $PACKAGE_PATH not found."
+      exit 1
+    fi
     ;;
   *)
     echo "Not on main branch, skipping NuGet package upload."
