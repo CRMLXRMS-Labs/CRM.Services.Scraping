5ce91b58c9735bbf9ff7929444ddf25fb67e0080
Andrii Voznesenskyi <voznesenskijandrej5@gmail.com>
2024-09-18 00:26:33 +0200

    (#10) repo: update workflow

---
+++ scripts/publish_nuget.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo Executing after success scripts on branch $GITHUB_REF_NAME
+echo Triggering NuGet package build
+
+cd src/CRMScraper.Library
+
+dotnet pack -c release /p:PackageVersion=1.1.$GITHUB_RUN_NUMBER --no-restore -o .
+
+echo Library package created for branch $GITHUB_REF_NAME
+
+case "$GITHUB_REF_NAME" in
+  "main")
+    echo Uploading CRMScraper.Library package to NuGet
+    dotnet nuget push *.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
+    ;;
+  *)
+    echo "Not on main branch, skipping NuGet package upload."
+    ;;
+esac
